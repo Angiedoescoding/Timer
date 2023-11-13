7a1777eaac5c0529cef698bbc8bd6f076084cd9e
Angiedoescoding <139031564+Angiedoescoding@users.noreply.github.com>
2023-11-12 21:30:40 -0800

    Update Clock.js

--- src/Clock.js
+++ src/Clock.js
@@ -1,39 +1,33 @@
-// function Clock() {
-//     return <p>HI - CLOCK</p>
-// }
-
-// export default Clock;
-
 
 import { Component } from "react";
 
 
-export class Clock extends Component {            // 1 - Project Initialization with setting the component (super and props, this.state)
+export class Clock extends Component {           
     constructor(props) {
         super(props);
 
         this.state={
-            time: new Date().toLocaleTimeString()    // .toLocaleTimeString() Возвращает сроку с представлением части со временем текущей даты (в PM и АМ, полностью ЧЧ:ММ:СС).
+            time: new Date().toLocaleTimeString()   
         }
     }
 
-    componentDidMount() {                       // 3 - Компонент узнал с помощью componentDidMount(), что мы должны обновлять время (наше состояние) каждую секунду 
-        this.timerID = setInterval(() =>        // 4 - Внyтри компонент Clock указал браузеру, что нужно настроить таймер для выхова метода tick()
-        this.tick(), 1000)                      // метод tick (рандомное название дали) вызывается каждую секунду (setInterval(функция, 1000 - каждую секунду)
+    componentDidMount() {                      
+        this.timerID = setInterval(() =>       
+        this.tick(), 1000)                     
     }
 
     componentWillUnmount() {
-        clearInterval(this.timerID)             // 6 -  Если в какой-то момент захотим остановить таймер, нужна эта команда (by default)
+        clearInterval(this.timerID)            
     }
 
-    tick() {                                    // 5 - Внтури него компонент Clock обновляет пользовательский интерфейс, вызывая setState(), т.е. обновляя наше состояние
+    tick() {                                   
         this.setState({
             time: new Date().toLocaleTimeString()      
         })
     }
 
-        render() {                              // 2 - Вызвался метод render() чтобы нам отобразить первоначальное состояние (статические время, которое еще не двигалось)
-            return(                             // React знает, что состояние изменилось, снова вызывая метод render()
+        render() {                             
+            return(                            
                 <div>                           
                     <p className="Time">{this.state.time}</p>
                 </div>
